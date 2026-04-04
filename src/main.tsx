940abf1a6647267d4961a908277fc72d5ca12e10
sheheenmtp <muhammadsheheen@gmail.com>
2026-04-04 13:03:43 +0530

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </>,
+);
